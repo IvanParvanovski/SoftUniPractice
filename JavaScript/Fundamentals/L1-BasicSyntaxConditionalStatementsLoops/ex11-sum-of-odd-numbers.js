fb6a95485a4c039163cf99124c17ce74ca3098d2
IvanParvanovski <iparvanovski@gmail.com>
2021-05-22 18:36:25 +0300

    Cleaner Solution

--- JavaScript/Fundamentals/L1-BasicSyntaxConditionalStatementsLoops/ex11-sum-of-odd-numbers.js
+++ JavaScript/Fundamentals/L1-BasicSyntaxConditionalStatementsLoops/ex11-sum-of-odd-numbers.js
@@ -1,21 +1,31 @@
-function sumOfOddNumbers(number) {
-    let counter = 0;
-    let startNumber = 1;
-    let total = 0;
+// function sumOfOddNumbers(number) {
+//     let counter = 0;
+//     let startNumber = 1;
+//     let total = 0;
 
-    while (true) {
-        if (counter === number) {
-            break;
-        }
+//     while (true) {
+//         if (counter === number) {
+//             break;
+//         }
         
-        if (startNumber % 2 != 0) {
-            console.log(startNumber);
+//         if (startNumber % 2 != 0) {
+//             console.log(startNumber);
 
-            total += startNumber;
-            counter++;
-        }
+//             total += startNumber;
+//             counter++;
+//         }
 
-        startNumber++;    
+//         startNumber++;    
+//     }
+//     console.log(`Sum: ${total}`);
+// }
+
+function sumOfOddNumbers(number) {
+    let total = 0;
+    for (let i = 1; number > 0; i += 2) {
+        number--;
+        total += i;
+        console.log(i);
     }
     console.log(`Sum: ${total}`);
 }
