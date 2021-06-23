1b82bfc94a4c07325434566f4afd49583bda36cb
Ivan Parvanovski <iparvanovski@gmail.com>
2021-06-23 21:22:30 +0300

    C# HashSets

---
+++ C#/Fundamentals/HashSets/HashSets/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> mySet = new HashSet<string>();
+            mySet.Add("C");
+            mySet.Add("C");
+            mySet.Add("B");
+            mySet.Add("A");
+            mySet.Add("B");
+            mySet.Add("A");
+            foreach (string symbol in mySet)
+            {
+                Console.WriteLine(symbol);
+            }
+
+            Console.WriteLine();
+            
+            SortedSet<string> sortedSet = new SortedSet<string>();
+            sortedSet.Add("C");
+            sortedSet.Add("C");
+            sortedSet.Add("B");
+            sortedSet.Add("A");
+            sortedSet.Add("B");
+            sortedSet.Add("A");
+            foreach (string symbol in sortedSet)
+            {
+                Console.WriteLine(symbol);
+            }
+        }
+    }
+    
+}
