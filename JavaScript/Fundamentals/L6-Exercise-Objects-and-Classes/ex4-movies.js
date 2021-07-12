b697a5b46b7c37a0ed05bd72137fe33303b7d764
Ivan Parvanovski <iparvanovski@gmail.com>
2021-07-12 20:46:42 +0300

    JS -> Objects and Classes Basics Exercise

---
+++ JavaScript/Fundamentals/L6-Exercise-Objects-and-Classes/ex4-movies.js
@@ -0,0 +1,13 @@
+function movies(input) {
+    let cinema = {};
+
+    for (let movie of input) {
+        let movieData = movie.split(' ');
+        
+        let command = movieData.shift();
+        let movieName = movieData.join(' ');
+
+       
+
+    }
+}
