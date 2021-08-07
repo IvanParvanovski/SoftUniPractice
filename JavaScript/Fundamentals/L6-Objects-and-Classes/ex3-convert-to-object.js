6272719226fbab8c98dc7d674609e5b6f38cac44
Ivan Parvanovski <iparvanovski@gmail.com>
2021-08-07 16:53:53 +0300

    Renamed directories

--- JavaScript/Fundamentals/L6-Objects-and-Classes/ex3-convert-to-object.js
+++
@@ -1,9 +0,0 @@
-function convertToObject(jsonObject) {
-    let result = JSON.parse(jsonObject);
-
-    for (let key of Object.keys(result)) {
-        console.log(`${key}: ${result[key]}`);
-    }
-}
-
-json('{"name": "George", "age": 40, "town": "Sofia"}');
