2695fb1a2e30d59fefe45da8792d2496c1b6fe60
Ivan Parvanovski <iparvanovski@gmail.com>
2021-10-10 22:58:17 +0300

    Underline words by key word and get matches count

--- JavaScript/Advanced/L4-Exercise-DOM/ex4-searchInList/search.js
+++ JavaScript/Advanced/L4-Exercise-DOM/ex4-searchInList/search.js
@@ -1,3 +1,25 @@
 function search() {
-   // TODO
+   const possibleTowns = Array.from(document.querySelectorAll('#towns li'));
+   const searchedText = document.getElementById('searchText').value;
+   const matchResult = document.getElementById('result');
+   let matches = 0;
+
+   for (const town of possibleTowns) {
+      town.style.textDecoration = 'none';
+      town.style.fontWeight = 'normal';
+   }
+
+   for (const town of possibleTowns) {
+      const townLower = town.textContent.toLowerCase();
+      const searchedTextLower = searchedText.toLowerCase();
+
+      if (townLower.includes(searchedTextLower)) {
+         town.style.textDecoration = 'underline';
+         town.style.fontWeight = 'bold';
+         matches++;
+      } 
+   }
+
+   matchResult.textContent = `${matches} matches found`;
 }
+
