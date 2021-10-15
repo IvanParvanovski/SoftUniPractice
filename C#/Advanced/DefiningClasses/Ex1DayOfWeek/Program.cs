43e65a136086e1d4e94310748b49b8584af3280a
Ivan Parvanovski <iparvanovski@gmail.com>
2021-10-15 22:55:35 +0300

    Needed setup to run the programs

---
+++ C#/Advanced/DefiningClasses/Ex1DayOfWeek/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex1DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime inputData = DateTime.ParseExact(Console.ReadLine()!, "d-M-yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(inputData.DayOfWeek);
+        }
+    }
+}
---
+++ C#/Advanced/DefiningClasses/Ex4RectangleClass/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ex4RectangleClass
+{
+    class Rectangle
+    {
+        private int width;
+        private int height;
+        private string color;
+            
+        public Rectangle(int width, int height, string color)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Color = color;
+        }
+
+        public int Width
+        {
+            get => width;
+            set => width = value;
+        }
+
+        public int Height
+        {
+            get => height;
+            set => height = value;
+        }
+
+        public string Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public double CalculateArea()
+        {
+            return this.width * this.height;
+        }
+
+        public override string ToString()
+        {
+            return $"Rect({Width}, {Height}, {Color}) has area {CalculateArea()}.";
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine()!);
+            int height = int.Parse(Console.ReadLine()!);
+            string color = Console.ReadLine();
+
+            Rectangle rect = new Rectangle(width, height, color);
+            Console.WriteLine(rect);
+        }
+    }
+}
