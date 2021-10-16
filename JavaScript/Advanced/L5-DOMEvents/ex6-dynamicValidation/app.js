232c0bad76ffdc6079ca5e2b0c970e973d523769
Ivan Parvanovski <iparvanovski@gmail.com>
2021-10-16 19:38:34 +0300

    JS DOM Events Lecture Exercises 6-7 solution

--- JavaScript/Advanced/L5-DOMEvents/ex6-dynamicValidation/app.js
+++ JavaScript/Advanced/L5-DOMEvents/ex6-dynamicValidation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+    const emailInput = document.getElementById('email');
+    emailInput.addEventListener('change', onChange);
+
+    function onChange(event) {
+        const regexPattern = /^\w+@\w+\.\w+$/;
+
+        const emailElement = event.target;
+        const emailValue = emailElement.value;
+
+        const match = regexPattern.test(emailValue);
+        if (match) {
+            emailElement.classList.remove('error');
+        } else {
+            emailElement.classList.add('error');
+        }
+    }
 }
--- JavaScript/Advanced/L5-DOMEvents/ex7-shoppingCart/solution.js
+++ JavaScript/Advanced/L5-DOMEvents/ex7-shoppingCart/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-   //TODO...
+   document.getElementsByClassName('shopping-cart')[0].addEventListener('click', onClick);
+   document.getElementsByClassName('checkout')[0].addEventListener('click', checkout);
+
+   const cart = [];
+   const output = document.getElementsByTagName('textarea')[0];
+   output.value = '';
+
+   function onClick(event) {
+      if (event.target.tagName == 'BUTTON' && event.target.classList.contains('add-product')) {
+         const product = event.target.parentNode.parentNode;
+         const name = product.querySelector('.product-title').textContent;
+         const price = Number(product.querySelector('.product-line-price').textContent);
+    
+         cart.push({
+            name,
+            price,
+         });
+
+         output.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+      }
+   }
+
+   function checkout() {
+      const products = new Set();
+      cart.forEach(p => products.add(p.name));
+      const total = cart.reduce((t, c) => t + c.price, 0);
+      output.value += `You bought ${[...products.keys()].join(', ')} for ${total.toFixed(2)}.`;
+   }
 }
