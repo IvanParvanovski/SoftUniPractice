ee920d4676ac30b3bd69fd868f60d7611b71ce70
Ivan Parvanovski <iparvanovski@gmail.com>
2021-10-20 11:26:21 +0300

    JS Advanced Unit Tests and Error Handling - Group tests with describe and create one main instance for all tests with beforeEach()

--- JavaScript/Advanced/L7-UnitTestingAndErrorHandling/ex7/test.js
+++ JavaScript/Advanced/L7-UnitTestingAndErrorHandling/ex7/test.js
@@ -2,129 +2,118 @@ const {expect} = require('chai');
 const {createCalculator} = require('./ex7-addSubtract');
 
 describe('Ex7 AddSubtract Tests', () => {
-    it('returns an object which contains add, subtract, get', () => {
-        const result = createCalculator();
-        
-        expect(Object.keys(result).includes('add')).to.be.true;
-        expect(Object.keys(result).includes('subtract')).to.be.true;
-        expect(Object.keys(result).includes('get')).to.be.true;
+    let instance = null;
+
+    beforeEach(() => {
+        instance = createCalculator();
     });
 
-    it('returns an object which contains functions', () => {
-        const result = createCalculator();
-        const argumentsTypes = Object
-                                .keys(result)
-                                .map((r) => typeof result[r] == 'function');
-                                
-        expect(argumentsTypes.every(x => x == true)).to.be.true;
+    describe('Test Returned Functions', () => {
+        it('returns an object which contains add, subtract, get', () => {
+            expect(instance).to.has.ownProperty('add');        
+            expect(instance).to.has.ownProperty('subtract');        
+            expect(instance).to.has.ownProperty('get');        
+        });
+
+        it('returns an object which contains functions', () => {
+            const argumentsTypes = Object
+                                    .keys(instance)
+                                    .map((r) => typeof instance[r] == 'function');
+                                    
+            expect(argumentsTypes.every(x => x == true)).to.be.true;
+        });
+
+        describe('Test "add" function', () => {
+            it('accepts an integer and adds it to the sum', () => {        
+                const currentSum = instance.get();
+                instance.add(5);
+                const newValue = instance.get();
+        
+                expect(currentSum + 5).to.equal(newValue);
+            });
+    
+            it('accepts a floating number and adds it to the sum', () => {        
+                const currentSum = instance.get();
+                instance.add(5.5);
+                const newValue = instance.get();
+        
+                expect(currentSum + 5.5).to.equal(newValue);
+            });
+
+            it('accepts an integer as string and adds it to the sum', () => {        
+                const currentSum = instance.get();
+                instance.add('5');
+                const newValue = instance.get();
+        
+                expect(currentSum + 5).to.equal(newValue);
+            });
+
+            it('accepts a floating number as string and adds it to the sum', () => {        
+                const currentSum = instance.get();
+                instance.add('5.5');
+                const newValue = instance.get();
+        
+                expect(currentSum + 5.5).to.equal(newValue);
+            });
+
+            it('returns NaN when the argument is a text-string', () => {        
+                instance.add('hi');
+                const newSum = instance.get();
+        
+                expect(newSum).to.be.NaN;
+            });
+        });
+
+        describe('Test "subtract" function', () => {
+            it('accepts an integer and subtracts it of the sum', () => {        
+                const currentSum = instance.get();
+                instance.add(5);
+                const newValue = instance.get();
+        
+                expect(currentSum + 5).to.equal(newValue);
+            });
+        
+            it('accepts a floating number and subtracts it of the sum', () => {        
+                const currentSum = instance.get();
+                instance.subtract(5.5);
+                const newValue = instance.get();
+        
+                expect(currentSum - 5.5).to.equal(newValue);
+            })
+        
+            it('accepts an integer as string and subtracts it of the sum', () => {        
+                const currentSum = instance.get();
+                instance.subtract('5');
+                const newValue = instance.get();
+        
+                expect(currentSum - 5).to.equal(newValue);
+            });
+            
+            it('accepts a floating number as string and subtracts it of the sum', () => {        
+                const currentSum = instance.get();
+                instance.subtract('5.5');
+                const newValue = instance.get();
+        
+                expect(currentSum - 5.5).to.equal(newValue);
+            });
+        
+            it('returns NaN when the argument is a string', () => {        
+                instance.subtract('hi');
+                const newSum = instance.get();
+        
+                expect(newSum).to.be.NaN;
+            });
+        });
+
+        describe('Test "get" function', () => {
+            it('get function returns the value of the internal sum', () => {
+                expect(instance.get()).to.equal(0);
+            });
+        });
     });
 
     it('has an internal sum that cannot be modified', () => {
-        const currentValue = createCalculator().value;
+        const currentValue = instance.value;
         expect(currentValue).to.equal(undefined);
-    });
-
-    it('add function accepts an integer and adds to the sum', () => {
-        const calculator = createCalculator();
-        
-        const currentSum = calculator.get();
-        calculator.add(5);
-        const newValue = calculator.get();
-
-        expect(currentSum + 5).to.equal(newValue);
-    });
-
-    it('add function accepts a floating number and adds it', () => {
-        const calculator = createCalculator();
-        
-        const currentSum = calculator.get();
-        calculator.add(5.5);
-        const newValue = calculator.get();
-
-        expect(currentSum + 5.5).to.equal(newValue);
-    })
-
-    it('add function accepts an integer as string and adds to the sum', () => {
-        const calculator = createCalculator();
-        
-        const currentSum = calculator.get();
-        calculator.add('5');
-        const newValue = calculator.get();
-
-        expect(currentSum + 5).to.equal(newValue);
-    });
-    
-    it('add function accepts a floating number as string and adds to the sum', () => {
-        const calculator = createCalculator();
-        
-        const currentSum = calculator.get();
-        calculator.add('5.5');
-        const newValue = calculator.get();
-
-        expect(currentSum + 5.5).to.equal(newValue);
-    });
-
-    it('add function returns NaN when the argument is a string', () => {
-        const calculator = createCalculator();
-        
-        calculator.add('hi');
-        const newSum = calculator.get();
-
-        expect(newSum).to.be.NaN;
-    });
-
-    it('subtract function accepts an integer and subtracts it of the sum', () => {
-        const calculator = createCalculator();
-        
-        const currentSum = calculator.get();
-        calculator.add(5);
-        const newValue = calculator.get();
-
-        expect(currentSum + 5).to.equal(newValue);
-    });
-
-    it('subtract function accepts a floating number and subtracts it of the sum', () => {
-        const calculator = createCalculator();
-        
-        const currentSum = calculator.get();
-        calculator.subtract(5.5);
-        const newValue = calculator.get();
-
-        expect(currentSum - 5.5).to.equal(newValue);
-    })
-
-    it('subtract function accepts an integer as string and subtracts it of the sum', () => {
-        const calculator = createCalculator();
-        
-        const currentSum = calculator.get();
-        calculator.subtract('5');
-        const newValue = calculator.get();
-
-        expect(currentSum - 5).to.equal(newValue);
-    });
-    
-    it('subtract function accepts a floating number as string and subtracts it of the sum', () => {
-        const calculator = createCalculator();
-        
-        const currentSum = calculator.get();
-        calculator.subtract('5.5');
-        const newValue = calculator.get();
-
-        expect(currentSum - 5.5).to.equal(newValue);
-    });
-
-    it('subtract function returns NaN when the argument is a string', () => {
-        const calculator = createCalculator();
-        
-        calculator.subtract('hi');
-        const newSum = calculator.get();
-
-        expect(newSum).to.be.NaN;
-    });
-
-    it('get function returns the value of the internal sum', () => {
-        const calculator = createCalculator();
-
-        expect(calculator.get()).to.equal(0);
     });
 });
