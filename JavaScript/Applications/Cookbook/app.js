1893b5209f68fa067666b0e9f409ccea0b492ca5
Ivan Parvanovski <iparvanovski@gmail.com>
2021-11-06 23:04:57 +0200

    JS Applications Workshop - Cookbook Part 1

---
+++ JavaScript/Applications/Cookbook/app.js
@@ -0,0 +1,104 @@
+function cookbook() {    
+    const mainElement = document.getElementsByTagName('main')[0];
+    displayArticles();
+
+    async function displayRecipe(event) {
+        const recipeId = this.id;
+        removePreviousRecipe();
+
+        const articleData = await getRequest(`http://localhost:3030/jsonstore/cookbook/details/${recipeId}`);
+        const newArticleDiv = document.createElement('article');
+    
+        const title = document.createElement('h2');
+        title.textContent = articleData.name;
+
+        // Band Div Elements
+        const bandDiv = document.createElement('div');
+        bandDiv.classList.add('band');
+
+        // ThumbDiv Elements
+        const thumbDiv = document.createElement('div');
+        thumbDiv.classList.add('thumb');
+        thumbDiv.innerHTML = `<img src="${articleData.img}">`;
+
+        // Ingredients
+        const ingredientsDiv = document.createElement('div');
+        ingredientsDiv.classList.add('ingredients');
+        ingredientsDiv.innerHTML = '<h3>Ingredients:</h3>';
+
+        const ingredientsUl = document.createElement('ul'); 
+        for (let ingredient of articleData.ingredients) {
+            const li = document.createElement('li');
+            li.textContent = ingredient;
+
+            ingredientsUl.appendChild(li);
+        }
+
+        ingredientsDiv.appendChild(ingredientsUl);
+
+        // Append Sections 
+        bandDiv.appendChild(thumbDiv);
+        bandDiv.appendChild(ingredientsDiv);
+
+        // Description Elements
+        const descriptionDiv = document.createElement('div');
+        descriptionDiv.classList.add('description');
+
+        descriptionDiv.innerHTML = '<h3>Preparation:</h3>';
+        
+        for (const text of articleData.steps) {
+            const paragraph = document.createElement('p');
+            paragraph.textContent = text;
+
+            descriptionDiv.appendChild(paragraph);
+        }
+        
+        newArticleDiv.appendChild(bandDiv);
+        newArticleDiv.appendChild(descriptionDiv);
+
+        mainElement.appendChild(newArticleDiv);
+    }
+
+    function removePreviousRecipe() {
+        const lastArticle = mainElement.lastElementChild;
+        if ([...lastArticle.classList].includes('preview')) {
+            return;
+        }
+
+        mainElement.removeChild(lastArticle);
+    }
+
+    async function displayArticles() {
+        const articles = await getRequest('http://localhost:3030/jsonstore/cookbook/recipes');
+
+        for (const [id, article] of Object.entries(articles)) {
+            const newArticle = document.createElement('article');
+            newArticle.className = 'preview';
+    
+            newArticle.innerHTML = [
+                '<div class="title">',
+                    `<h2>${article.name}</h2>`,
+                '</div>',
+                '<div class="small">',
+                    `<img src="${article.img}">`,
+                '</div>'
+            ].join('');
+
+            newArticle.addEventListener('click', displayRecipe.bind({id}));
+            mainElement.appendChild(newArticle);
+        }
+    }
+
+    async function getRequest(url) {
+        const response = await fetch(url);
+
+        if (response.status != 200) {
+            throw new Error(`Articles: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+    }
+
+
+}
