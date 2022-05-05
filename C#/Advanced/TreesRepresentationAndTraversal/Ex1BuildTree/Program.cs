a0fa7097eb04414219163a500735f5ffa6a03d3f
Ivan Parvanovski <iparvanovski@gmail.com>
2022-05-05 22:48:11 +0300

    Svetlina C# -> Traversal

--- C#/Advanced/TreesRepresentationAndTraversal/Ex1BuildTree/Program.cs
+++ C#/Advanced/TreesRepresentationAndTraversal/Ex1BuildTree/Program.cs
@@ -7,20 +7,40 @@ namespace Ex1BuildTree
     {
         public static void Main(string[] args)
         {
-            TreeNode<string> tree = new TreeNode<string>("A", 
-                new TreeNode<string>("B", 
-                    new TreeNode<string>("D",
-                        new TreeNode<string>("H"), 
-                        new TreeNode<string>("I")),
-                    new TreeNode<string>("E")
-                ),
-                new TreeNode<string>("C",
-                    new TreeNode<string>("F",
-                        new TreeNode<string>("J")),
-                    new TreeNode<string>("G"))
-            );
-            
-            Console.WriteLine(tree.ToString());
+            // TreeNode<string> tree = new TreeNode<string>("A", 
+            //     new TreeNode<string>("B", 
+            //         new TreeNode<string>("D",
+            //             new TreeNode<string>("H"), 
+            //             new TreeNode<string>("I")),
+            //         new TreeNode<string>("E")
+            //     ),
+            //     new TreeNode<string>("C",
+            //         new TreeNode<string>("F",
+            //             new TreeNode<string>("J")),
+            //         new TreeNode<string>("G"))
+            // );
+            //
+            // Console.WriteLine(tree.ToString());
+
+            TreeNode<int> tree2 = new TreeNode<int>(25,
+                new TreeNode<int>(20, 
+                    new TreeNode<int>(10,
+                        new TreeNode<int>(5,
+                            new TreeNode<int>(1),
+                            new TreeNode<int>(8)),
+                        new TreeNode<int>(12,
+                            new TreeNode<int>(15))),
+                    new TreeNode<int>(22)),
+                new TreeNode<int>(36,
+                    new TreeNode<int>(30,
+                        new TreeNode<int>(28)),
+                    new TreeNode<int>(40,
+                        new TreeNode<int>(38),
+                        new TreeNode<int>(48,
+                            new TreeNode<int>(45),
+                            new TreeNode<int>(50)))));
+
+            Console.WriteLine(tree2.ToString());
         }
     }
 }
